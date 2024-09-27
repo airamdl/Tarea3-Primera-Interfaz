5724e0fd3898d857a5cdb8500d50ffe30f353227
airamdl <airamdl2001@gmail.com>
2024-09-27 09:07:55 +0100

    Show images

--- main.py
+++ main.py
@@ -2,10 +2,9 @@
 #Mostrar una imagen con un cuadro de texto y un botón comprobar.
 #La imagen es un caracter hiragana del idioma japonés, el nombre del fichero correspondiente a la imagen es la traducción al español (sin la extensión).
 #Se deberá comprobar que ha introducido la traducción correctamente en el cuadro de texto, así con las 10 imágenes aleatorias y que no se repitan.
+from fnmatch import translate
 
-
-
-
+import PIL
 from PIL import Image, ImageTk
 import tkinter as tk
 from tkinter import ttk
@@ -14,26 +13,30 @@ import random
 
 
 
-def cargar_imagenes(hiragana):
+def get_hiragana_images_and_translates(hiragana):
     images = []
     translates = []
     for file in os.listdir(hiragana):
         if file.endswith(".png"):
             translate = file[:-4]
-            ruta_imagen = os.path.join(hiragana, file)
-            images.append(ruta_imagen)
+            img_route = os.path.join(hiragana, file)
+            images.append(img_route)
             translates.append(translate)
     return images, translates
 
 
+def show_images(imagen, translate):
+    global window, label_image, entry_answer, check_button
 
+    img = PIL.Image.open(imagen)
+    img = img.resize((200, 200))
+    photo = PIL.ImageTk.PhotoImage(img)
 
+    label_image.config(image=photo)
+    label_image.image = photo
 
-
-
-
-
-
+    entry_answer.delete(0, tk.END)
+    entry_answer.focus()
 
 
 
